28377c4b19eaa0c5ca8aacb08b75532c4392970f
Franciszek Janicki <franciszek.janicki1@gmail.com>
2025-07-05 20:11:08 +0200

    init

---
+++ atlas_common.h
@@ -0,0 +1,13 @@
+#ifndef ATLAS_COMMON_ATLAS_COMMON_H
+#define ATLAS_COMMON_ATLAS_COMMON_H
+
+#include "atlas_config.h"
+#include "atlas_data.h"
+#include "atlas_err.h"
+#include "atlas_joint_num.h"
+#include "atlas_log.h"
+#include "atlas_packet.h"
+#include "atlas_path.h"
+#include "atlas_utility.h"
+
+#endif // ATLAS_COMMON_ATLAS_COMMON_H
---
+++ atlas_log.h
@@ -0,0 +1,9 @@
+#ifndef ATLAS_COMMON_ATLAS_LOG_H
+#define ATLAS_COMMON_ATLAS_LOG_H
+
+#include <stdarg.h>
+#include <stddef.h>
+
+void atlas_log(char const* format, ...);
+
+#endif // ATLAS_COMMON_ATLAS_LOG_H
