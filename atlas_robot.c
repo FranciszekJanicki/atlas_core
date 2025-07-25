ec29dde441a48f3947899a381170966a188310d0
Franciszek Janicki <franciszek.janicki1@gmail.com>
2025-07-26 00:04:16 +0200

    changes

--- atlas_robot.c
+++ atlas_robot.c
@@ -1,40 +1,9 @@
 #include "atlas_robot.h"
+#include "atlas_data.h"
 #include "atlas_joint.h"
 #include "atlas_log.h"
 #include "atlas_utility.h"
 #include <stdint.h>
-
-static bool atlas_robot_
-_is_equal(
-    atlas_robot_joints_data_t const* joints1,
-    atlas_robot_joints_data_t const* joints2)
-{
-    ATLAS_ASSERT(joints1 != NULL);
-    ATLAS_ASSERT(joints2 != NULL);
-
-    for (uint8_t num = 0; num < ATLAS_JOINT_NUM; ++num) {
-        if (joints1->positions[num] != joints2->positions[num]) {
-            return false;
-        }
-    }
-
-    return true;
-}
-
-static bool atlas_robot_cartesian_data_is_equal(
-    atlas_robot_cartesian_data_t const* cartesian1,
-    atlas_robot_cartesian_data_t const* cartesian2)
-{
-    ATLAS_ASSERT(cartesian1 != NULL);
-    ATLAS_ASSERT(cartesian2 != NULL);
-
-    return cartesian1->payload.position.x == cartesian2->position.x &&
-           cartesian1->position.y == cartesian2->position.y &&
-           cartesian1->position.z == cartesian2->position.z &&
-           cartesian1->orientation.x == cartesian2->orientation.x &&
-           cartesian1->orientation.y == cartesian2->orientation.y &&
-           cartesian1->orientation.z == cartesian2->orientation.z;
-}
 
 bool atlas_robot_data_is_equal(atlas_robot_data_t const* data1,
                                atlas_robot_data_t const* data2)
@@ -44,40 +13,9 @@ bool atlas_robot_data_is_equal(atlas_robot_data_t const* data1,
 
     if (data1->type == ATLAS_ROBOT_DATA_TYPE_JOINTS &&
         data2->type == ATLAS_ROBOT_DATA_TYPE_JOINTS) {
-        return atlas_robot_joints_data_is_equal(&data1->payload.joints,
-                                                &data2->payload.joints);
+        return atlas_joints_data_is_equal(&data1->payload.joints,
+                                          &data2->payload.joints);
     }
-}
-
-static void atlas_robot_cartesian_data_print(
-    atlas_robot_cartesian_data_t const* cartesian)
-{
-    ATLAS_ASSERT(cartesian != NULL);
-
-    atlas_robot_log(
-        "position x: %d, y: %d, z: %d orientation x: %d, y: %d, z: %d "
-        "[deg * 100]\n\r",
-        (int32_t)cartesian->position.x * 100,
-        (int32_t)cartesian->position.y * 100,
-        (int32_t)cartesian->position.z * 100,
-        (int32_t)cartesian->orientation.x * 100,
-        (int32_t)cartesian->orientation.y * 100,
-        (int32_t)cartesian->orientation.z * 100);
-}
-
-static void atlas_robot_joints_data_print(
-    atlas_robot_joints_data_t const* joints)
-{
-    ATLAS_ASSERT(joints != NULL);
-
-    atlas_robot_log(
-        "position 1: %d, 2: %d, 3: %d, 4: %d, 5: %d, 6: %d [deg * 100]\n\r",
-        (int32_t)joints->positions[ATLAS_JOINT_NUM_1] * 100,
-        (int32_t)joints->positions[ATLAS_JOINT_NUM_2] * 100,
-        (int32_t)joints->positions[ATLAS_JOINT_NUM_3] * 100,
-        (int32_t)joints->positions[ATLAS_JOINT_NUM_4] * 100,
-        (int32_t)joints->positions[ATLAS_JOINT_NUM_5] * 100,
-        (int32_t)joints->positions[ATLAS_JOINT_NUM_6] * 100);
 }
 
 void atlas_robot_data_print(atlas_robot_data_t const* data)
@@ -85,9 +23,9 @@ void atlas_robot_data_print(atlas_robot_data_t const* data)
     ATLAS_ASSERT(data != NULL);
 
     if (data->type == ATLAS_ROBOT_DATA_TYPE_JOINTS) {
-        atlas_robot_joints_data_print(&data->payload.joints);
+        atlas_joints_data_print(&data->payload.joints);
     } else if (data->type == ATLAS_ROBOT_DATA_TYPE_CARTESIAN) {
-        atlas_robot_cartesian_data_print(&data->payload.cartesian);
+        atlas_cartesian_data_print(&data->payload.cartesian);
     }
 }
 
